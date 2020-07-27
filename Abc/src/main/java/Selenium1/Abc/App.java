080c7cfe24a59e11e77f9dbec9050015473e09b1
arunagiri <arunagiri@LAPTOP-G66AQCG6>
2020-07-27 16:39:06 +0530

    1st

--- Abc/src/main/java/Selenium1/Abc/App.java
+++ Abc/src/main/java/Selenium1/Abc/App.java
@@ -4,10 +4,8 @@ package Selenium1.Abc;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
